c366309791ae3b6b491e73e4b90aa8da60b04178
sainani8897 <sainani8897@gitlab.com>
2022-12-14 08:04:50 +0530

    dropzone changes

--- src/components/Dropzone/index.js
+++ src/components/Dropzone/index.js
@@ -1,69 +1,149 @@
-
-import React, {useMemo} from 'react';
-import { useDropzone } from 'react-dropzone';
+import React, { useMemo, useEffect, useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
 
 const baseStyle = {
   flex: 1,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  padding: '20px',
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
   borderWidth: 2,
   borderRadius: 2,
-  borderColor: '#eeeeee',
-  borderStyle: 'dashed',
-  backgroundColor: '#fafafa',
-  color: '#bdbdbd',
-  outline: 'none',
-  transition: 'border .24s ease-in-out'
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
 };
 
 const focusedStyle = {
-  borderColor: '#2196f3'
+  borderColor: "#2196f3",
 };
 
 const acceptStyle = {
-  borderColor: '#00e676'
+  borderColor: "#00e676",
 };
 
 const rejectStyle = {
-  borderColor: '#ff1744'
+  borderColor: "#ff1744",
+};
+
+const thumbsContainer = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  marginTop: 16
+};
+
+const thumb = {
+  display: 'inline-flex',
+  borderRadius: 2,
+  border: '1px solid #eaeaea',
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: 'border-box'
+};
+
+const thumbInner = {
+  display: 'flex',
+  minWidth: 0,
+  overflow: 'hidden'
+};
+
+const img = {
+  display: 'block',
+  width: 'auto',
+  height: '100%'
 };
 
 const DropzoneHandler = (props) => {
-
   console.log(props);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [selectedFiles, setFiles] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+   const viewFiles =  acceptedFiles.map((file) =>{ 
+      console.log(file);
+      return Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      })
+    }
+    )
+    console.log(viewFiles);
+    setFiles(viewFiles);
+  },[]);
 
   const {
+    acceptedFiles,
     getRootProps,
     getInputProps,
     isFocused,
     isDragAccept,
-    isDragReject
-  } = useDropzone({accept: {'image/*': []}});
+    isDragReject,
+  } = useDropzone({
+    accept: { "image/*": [] },
+    onDrop,
+  });
 
-  const style = useMemo(() => ({
-    ...baseStyle,
-    ...(isFocused ? focusedStyle : {}),
-    ...(isDragAccept ? acceptStyle : {}),
-    ...(isDragReject ? rejectStyle : {})
-  }), [
-    isFocused,
-    isDragAccept,
-    isDragReject
-  ])
+  // File Upload
+  const uploadFile = async (file) => {
+    const files = await axios.post(
+      process.env.REACT_APP_API_URL + "/media-manager",
+      { file },
+      { headers: { Authorization: localStorage.getItem("token") ?? null } }
+    );
+  };
+
+  const files = acceptedFiles.map((file) => {
+    return (
+      <li key={file.path}>
+        {file.path} - {file.size} bytes
+      </li>
+    );
+  });
+
+  const thumbs = selectedFiles?.map(file => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img
+          src={file.preview}
+          style={img}
+          // Revoke data uri after image is loaded
+          onLoad={() => { URL.revokeObjectURL(file.preview) }}
+        />
+      </div>
+    </div>
+  ));
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
   return (
     <section className="container">
-      <div {...getRootProps({ className: 'dropzone disabled' })}>
+      <div {...getRootProps({ style })}>
         <input {...getInputProps()} />
         <p>Drag 'n' drop some files here, or click to select files</p>
       </div>
-      <aside>
+      {/* <aside>
         <h4>Files</h4>
         <ul>{files}</ul>
+      </aside> */}
+       <aside style={thumbsContainer}>
+        {thumbs}
       </aside>
     </section>
   );
-}
+};
 
-export default DropzoneHandler
+export default DropzoneHandler;
